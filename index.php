3822cd0b4a30e417e7fc1554ca0da4261d82d523
tanyabednyakova <tmbednykova@yandex.ru>
2023-02-14 14:50:30 +0300

    add content to index.php

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
     <title>Веб-приложение, часть 1</title>
 </head>
 <body>
+    <header>
+        <h1>Разработка веб-приложений, часть 1.</h1>
+    </header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html">Главная</a>
+            </li>
+            <li>
+                <a href="reg.html">Регистраиция</a>
+            </li>
+            <li>
+                <a href="auth.html">Авторизация</a>
+            </li>
+            <li>
+                <a href="hidden_page.html">Скрытая страница</a>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <div>
+        Пример регистрации, авторизации на PHP.
+        </div>
+    </main>
+    <footer>
+        <h4>Фамилия Имя, группа</h4>
+    </footer>
     
 </body>
 </html>
